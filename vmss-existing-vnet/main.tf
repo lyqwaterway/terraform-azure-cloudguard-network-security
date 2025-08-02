48739b55e649c0485621c63e25ceedb4b020b5a6
yongqiangliu <343847055@qq.com>
2025-08-02 21:05:39 +0800

    remove provider

--- vmss-existing-vnet/main.tf
+++ vmss-existing-vnet/main.tf
@@ -1,14 +1,3 @@
-provider "azurerm" {
-  subscription_id = var.subscription_id
-  client_id = var.client_id
-  client_secret = var.client_secret
-  tenant_id = var.tenant_id
-  environment = var.environment
-  skip_provider_registration = true
-
-  features {}
-}
-
 //********************** Basic Configuration **************************//
 module "common" {
   source = "../common"
